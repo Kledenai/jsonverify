1c9e36528aa8c95784ed007d0e62d8de8fe74db4
kledenai <bruno@khaneland.com>
2025-01-10 15:49:01 -0300

    updating types config

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,20 @@
 export type PrimitiveType = 'string' | 'number' | 'boolean' | 'null';
 
 export interface SchemaObject {
-  [key: string]: PrimitiveType | SchemaObject | SchemaObject[];
+  type?: PrimitiveType | 'object' | 'array';
+  properties?: Record<
+    string,
+    Schema & {
+      optional?: boolean;
+    }
+  >;
+  items?: Schema | Schema[];
+  required?: string[];
+  additionalProperties?: boolean;
+  [key: string]: any;
 }
+
+export type JsonSchemaType = 'string' | 'number' | 'boolean' | 'null' | 'object' | 'array';
 
 export type Schema = PrimitiveType | SchemaObject | SchemaObject[];
 export type Data = Record<string, any>;
